64e3a873568b24a7c273481a7e301f7e15a081a0
Loic Ho-Von <loic.hovon@gmail.com>
2021-11-07 19:48:34 -0500

    cleanup

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -185,7 +185,7 @@ def train_mlp(
         monitor="val_loss", min_delta=0.00, patience=patience, verbose=True, mode="min"
     )
     trainer = pl.Trainer(
-        default_root_dir=Path(model.root),
+        default_save_path=Path(model.root),
         max_epochs=max_epochs,
         early_stop_callback=early_stop_callback,
     )
