9b6a582d4f1f5ddb849e92405b8c6c2403019567
Loic Ho-Von <loic.hovon@gmail.com>
2021-11-26 21:22:07 -0500

    fixed bacth size, made validation optional

--- run_gcn.py
+++ run_gcn.py
@@ -12,31 +12,45 @@ from sklearn.metrics import r2_score, mean_squared_error
 
 from src.data import Dataset
 from src.utils import filter_preds_test_by_mask
-from src.config import MODELS_PATH
 
 import warnings
 
 warnings.simplefilter("ignore", RuntimeWarning)
 
 
-def explore_model_params(num_epochs=50, device="cpu"):
+def explore_model_params(num_epochs=50, device="cpu", validation=False):
 
-    trainset = TrainValDataset(
-        is_val=False, val_ratio=0.07, flatten=False, input_noise_scale=0.1, target_noise_scale=0.1
-    )
-    valset = TrainValDataset(
-        is_val=True, val_ratio=0.07, flatten=False, input_noise_scale=0.1, target_noise_scale=0.1
-    )
+    if validation:
+        trainset = TrainValDataset(
+            is_val=False,
+            val_ratio=0.07,
+            flatten=False,
+            input_noise_scale=0.1,
+            target_noise_scale=0.1,
+        )
+
+        valset = TrainValDataset(
+            is_val=True,
+            val_ratio=0.07,
+            flatten=False,
+            input_noise_scale=0.1,
+            target_noise_scale=0.1,
+        )
+        val_size = len(valset)
+    else:
+        trainset = Dataset(
+            is_test=False, flatten=False, input_noise_scale=0.1, target_noise_scale=0.1
+        )
+        val_size = 0
+
     testset = Dataset(is_test=True, flatten=False)
 
     train_size = len(trainset)
-    val_size = len(valset)
     test_size = len(testset)
 
     # Fill in parameters to explore
     # Will automatically explore all combinations
     params = OrderedDict(
-        concat_noisy_to_normal=[False],
         lr=[0.01, 0.005, 0.001],
         hid_dim=[100, 150, 250, 350],
         num_layer=[2],
@@ -53,24 +67,9 @@ def explore_model_params(num_epochs=50, device="cpu"):
             print(f"Already ran {run}\nSkipping")
             continue
 
-        # Optionally concatenate the normal and noisy data together
-        # By default we train and validate on
-        if run.concat_noisy_to_normal:
-            trainset_normal = TrainValDataset(is_val=False, val_ratio=0.07, flatten=False)
-            valset_normal = TrainValDataset(is_val=True, val_ratio=0.07, flatten=False)
+        trainloader = DataLoader(trainset, batch_size=run.batch_size)
 
-            # Add the noisy to the normal dataset to augment it
-            trainloader = DataLoader(
-                ConcatDataset([trainset, trainset_normal]), batch_size=run.batch_size
-            )
-            valloader = DataLoader(
-                ConcatDataset([valset, valset_normal]), batch_size=run.batch_size
-            )
-
-            train_size += len(trainset_normal)
-            val_size += len(valset_normal)
-        else:
-            trainloader = DataLoader(trainset, batch_size=run.batch_size)
+        if validation:
             valloader = DataLoader(valset, batch_size=run.batch_size)
 
         testloader = DataLoader(testset, batch_size=run.batch_size)
@@ -97,13 +96,13 @@ def explore_model_params(num_epochs=50, device="cpu"):
 
         print(f"Running model on: {next(model.parameters()).device}")
 
-        for _ in range(50):
+        for _ in range(num_epochs):
 
             # Puts the model in training mode
             # and activates gradients
             m.begin_epoch()
 
-            for i, (X, y_true, mask) in enumerate(trainloader):
+            for _, (X, y_true, mask) in enumerate(trainloader):
                 y_pred = model(X)
                 y_true, y_pred = filter_preds_test_by_mask(y_pred, y_true, mask)
                 loss = criterion(y_pred, y_true)
@@ -120,44 +119,50 @@ def explore_model_params(num_epochs=50, device="cpu"):
             torch.no_grad()
             model.eval()
 
-            # Validate
-            val_r2 = []
-            val_mse = 0
-            for i, (X, y_true, mask) in enumerate(valloader):
-                y_pred = model(X)
-                y_true, y_pred = filter_preds_test_by_mask(y_pred, y_true, mask)
-                val_r2.append(r2_score(y_true.data, y_pred.data))
-                val_mse += mean_squared_error(y_true.data, y_pred.data) * run.batch_size
+            if validation:
+                # Validate
+                val_r2 = []
+                val_mse = 0
+                for _, (X, y_true, mask) in enumerate(valloader):
+                    y_pred = model(X)
+                    y_true, y_pred = filter_preds_test_by_mask(y_pred, y_true, mask)
+                    # Multiply the MSE by each batch size, we will divide by n after
+                    val_mse += mean_squared_error(y_true.data, y_pred.data) * X.shape[0]
+                    val_r2.append(r2_score(y_true.data, y_pred.data))
 
-            val_mse = val_mse / len(valset)
-            val_r2 = np.mean(val_r2)
-            num_edges = len(torch.nonzero(model.A.detach(), as_tuple=False))
-            m.end_epoch(num_edges, val_mse, val_r2)
+                val_mse = val_mse / len(valset)
+                val_r2 = np.mean(val_r2)
+                num_edges = len(torch.nonzero(model.A.detach(), as_tuple=False))
+                m.end_epoch(num_edges, val_mse, val_r2)
 
         # Evaluate model on test set
         test_mse = 0
         test_r2 = []
-        for i, (X, y_true, mask) in enumerate(testloader):
+        for _, (X, y_true, mask) in enumerate(testloader):
             y_pred = model(X)
             y_true, y_pred = filter_preds_test_by_mask(y_pred, y_true, mask)
-            test_r2.append(r2_score(y_true.data, np.nan_to_num(y_pred.data)))
-            test_mse += mean_squared_error(y_true.data, y_pred.data) * run.batch_size
 
-        test_r2 = round(np.nan_to_num(np.mean(test_r2)), 2)
+            test_mse += mean_squared_error(y_true.data, y_pred.data) * X.shape[0]
+            test_r2.append(r2_score(y_true.data, y_pred.data))
+
         test_mse = round(test_mse / len(testset), 2)
+        test_r2 = round(np.mean(test_r2), 2)
+
         print(f"Test Set:\n\tMSE {test_mse}\n\tR2 {test_r2}\n")
 
-        if test_mse < 500:
-            print(f"Test set MSE of {test_mse} is under 500. Saving model.")
-            MODELS_FILENAME = f"GCN-{num_epochs}epochs-TestMSE={test_mse}-{run}.pth"
-            torch.save(
-                model,
-                MODELS_PATH / MODELS_FILENAME,
-            )
+        # if test_mse < 500:
+        #     print(f"Test set MSE of {test_mse} is under 500. Saving model.")
+        #     MODELS_FILENAME = f"GCN-{num_epochs}epochs-TestMSE={test_mse}-{run}.pth"
+        #     torch.save(
+        #         model,
+        #         MODELS_PATH / MODELS_FILENAME,
+        #     )
 
         m.end_run(test_mse)
 
 
 if __name__ == "__main__":
 
-    explore_model_params(num_epochs=50, device="cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    explore_model_params(num_epochs=1, device=device)
